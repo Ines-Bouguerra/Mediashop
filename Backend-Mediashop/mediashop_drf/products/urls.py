56310c7ff56935fc27ee141827e114d167a16046
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-02-18 17:42:14 +0100

    Add product_list namespace

--- Backend-Mediashop/mediashop_drf/products/urls.py
+++ Backend-Mediashop/mediashop_drf/products/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from products import views
 
 urlpatterns = [
-    url(r'^api/product$', views.product_list),
+    url(r'^api/product$', views.product_list,name='product_list'),
 
 
 ]
