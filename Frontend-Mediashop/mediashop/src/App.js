cbdb939e623ad14f15cb017f90f68b419f6ff94f
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-02-24 00:20:40 +0100

    Add home component

--- Frontend-Mediashop/mediashop/src/App.js
+++ Frontend-Mediashop/mediashop/src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Home from './components/home/Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div class="super_container">
-        <Header />
-        <Footer />
-        
-      </div>
+      <Router>
+        <div class="super_container">
+          <Header />
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
