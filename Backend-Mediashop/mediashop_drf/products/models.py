604aadf6d505f86aea3a18d9736fa55647f30036
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-02-28 13:45:15 +0100

    Fix models issues

--- Backend-Mediashop/mediashop_drf/products/models.py
+++ Backend-Mediashop/mediashop_drf/products/models.py
@@ -4,30 +4,26 @@ from django.contrib.auth.models import User
 
 
 class Product(models.Model):
-    category = models.CharField(max_length=255, blank=False)
+    category = models.CharField(max_length=255)
     description = models.TextField(max_length=255)
     domaine = models.CharField(max_length=255)
-    name = models.CharField(max_length=255, blank=False)
+    name = models.CharField(max_length=255)
     reference = models.TextField()
     discount = models.CharField(max_length=255)
-    url = models.URLField(blank=False)
+    url = models.URLField()
     timestamp = models.DateField(auto_now_add=True)
-    brand = models.CharField(max_length=255, blank=False)
+    brand = models.CharField(max_length=255)
     priceString = models.CharField(max_length=255)
     retailer = models.CharField(max_length=255)
     marketplace = models.CharField(max_length=255)
-    price = models.FloatField(blank=False)
+    price = models.FloatField()
     currency = models.CharField(max_length=3)
     sub_category = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
-    short_description = models.TextField(max_length=255, unique=True)
+    short_description = models.TextField(max_length=255)
     old_price = models.FloatField()
-    image = models.URLField(blank=False)
-    marketplaceId = models.CharField(max_length=255, blank=False)
-
-    class Meta:
-        verbose_name_plural = 'Products'
-        ordering = ('timestamp',)
+    image = models.URLField()
+    marketplaceId = models.CharField(max_length=255)
 
     def __str__(self):
         return '%s' % self.name
@@ -40,18 +36,12 @@ class Favorite(models.Model):
     author = models.CharField(max_length=255, default='admin')
     created_date = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        verbose_name_plural = 'Favorites'
-
 
 class Post(models.Model):
     post_created_by = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="user_creator_post")
     post_text = models.CharField(max_length=255, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name_plural = 'Posts'
 
     def __str__(self):
         return '%s' % self.post_text
--- Backend-Mediashop/mediashop_drf/products/serializers.py
+++ Backend-Mediashop/mediashop_drf/products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from products.models import Product
+from products.models import *
 
 
 class products_Serializer(serializers.ModelSerializer):
@@ -28,3 +28,17 @@ class products_Serializer(serializers.ModelSerializer):
                   'image',
                   'marketplaceId',
                   )
+        verbose_name_plural = 'Products'
+        ordering = ('timestamp', 'price')
+
+        model = Favorite
+        fields = ('favorite_created_by', 'post_text', 'created_at')
+        verbose_name_plural = 'Favorites'
+
+        model = Rating
+        fields = ('rating_created_by', 'created_at')
+        verbose_name_plural = 'Ratings'
+
+        model = Post
+        fields = ('post_text', 'created_at', 'post_created_by')
+        verbose_name_plural = 'Posts'
