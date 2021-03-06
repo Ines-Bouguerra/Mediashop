b4f872f6c9ff7d8d23e62e7df8c8a2c8b754a170
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-03-06 07:52:48 +0100

    Use proxy

--- Frontend-Mediashop/src/components/products/ProductList.js
+++ Frontend-Mediashop/src/components/products/ProductList.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
-const api = axios.create({
-    baseURL: 'http://localhost:8080/api/products/'
-})
+
 export default class ProductList extends Component {
     state = {
         products: []
     }
     constructor() {
         super();
-        api.get('/product_list').then(res => {
+        axios.get('/api/products/product_list').then(res => {
             console.log(res.data)
             this.setState({ products: res.data })
         })
