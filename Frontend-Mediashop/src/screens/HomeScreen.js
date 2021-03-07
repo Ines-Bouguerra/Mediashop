00997a9bd9ab8c8a94e60ed5b48f09d1acb32813
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-03-07 17:07:09 +0100

    Add Category's components & screens

--- Frontend-Mediashop/src/screens/HomeScreen.js
+++ Frontend-Mediashop/src/screens/HomeScreen.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
-import {Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import Category from '../components/category/Category'
-import products from  '../products'
+import categories from '../category'
 export default class HomeScreen extends Component {
     render() {
         return (
             <Container>
                 <Row>
-                    {products.map(product => (
+                    {categories.map(category => (
                         <Col sm={12} md={6} lg={4} xl={3}>
-                            <Category product={product}/>
+                            <Category category={category} />
                         </Col>
                     ))}
                 </Row>
