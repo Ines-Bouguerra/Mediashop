36ae1390322b702a5b3099703c3afd4f18e88cdc
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-03-14 21:48:31 +0100

    Setup logout

--- Frontend-Mediashop/src/components/header/Header.js
+++ Frontend-Mediashop/src/components/header/Header.js
@@ -1,12 +1,53 @@
 // Header.js
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { logout } from "../../actions/auth";
-const Header = () => {
+
+const Header = ({ logout, isAuthenticated }) => {
+  const [redirect, setRedirect] = useState(false);
+
+  const logout_user = () => {
+    logout();
+    setRedirect(true);
+  };
+
+  const guestLinks = () => (
+    <Fragment>
+      <div className="top_bar_content ml-auto">
+        <div className="top_bar_user">
+          <div className="user_icon">
+            <img src="images/user.svg" alt="" />
+          </div>
+          <div>
+            <Link to="/Register">Register</Link>
+          </div>
+          <div>
+            <Link to="/Login">Sign in</Link>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+
+  const authLinks = () => (
+    <div className="top_bar_content ml-auto">
+      <div className="top_bar_user">
+      <div className="user_icon">
+            <img src="images/logout.png" alt="" />
+          </div>
+        <div>
+          <Link to="/" onClick={logout_user}>
+            Logout
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
-    <div>
+    <Fragment>
       {/* Header */}
       <header className="header">
         {/* Top Bar */}
@@ -28,19 +69,7 @@ const Header = () => {
                     mediashop@medianet.com.tn
                   </a>
                 </div>
-                <div className="top_bar_content ml-auto">
-                  <div className="top_bar_user">
-                    <div className="user_icon">
-                      <img src="images/user.svg" alt="" />
-                    </div>
-                    <div>
-                      <Link to="/Register">Register</Link>
-                    </div>
-                    <div>
-                      <Link to="/Login">Sign in</Link>
-                    </div>
-                  </div>
-                </div>
+                {isAuthenticated ? authLinks() : guestLinks()}
               </div>
             </div>
           </div>
@@ -355,257 +384,8 @@ const Header = () => {
             </div>
           </div>
         </nav>
-        {/* Menu */}
-        <div className="page_menu">
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <div className="page_menu_content">
-                  <div className="page_menu_search">
-                    <form action="#">
-                      <input
-                        type="search"
-                        required="required"
-                        className="page_menu_search_input"
-                        placeholder="Search for products..."
-                      />
-                    </form>
-                  </div>
-                  <ul className="page_menu_nav">
-                    <li className="page_menu_item has-children">
-                      <Link to="/">
-                        Language
-                        <i className="fa fa-angle-down" />
-                      </Link>
-                      <ul className="page_menu_selection">
-                        <li>
-                          <Link to="/">
-                            English
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Italian
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Spanish
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Japanese
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                      </ul>
-                    </li>
-                    <li className="page_menu_item has-children">
-                      <Link to="/">
-                        Currency
-                        <i className="fa fa-angle-down" />
-                      </Link>
-                      <ul className="page_menu_selection">
-                        <li>
-                          <Link to="/">
-                            US Dollar
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            EUR Euro
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            GBP British Pound
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            JPY Japanese Yen
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                      </ul>
-                    </li>
-                    <li className="page_menu_item">
-                      <Link to="/">
-                        Home
-                        <i className="fa fa-angle-down" />
-                      </Link>
-                    </li>
-                    <li className="page_menu_item has-children">
-                      <Link to="/">
-                        Super Deals
-                        <i className="fa fa-angle-down" />
-                      </Link>
-                      <ul className="page_menu_selection">
-                        <li>
-                          <Link to="/">
-                            Super Deals
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li className="page_menu_item has-children">
-                          <Link to="/">
-                            Menu Item
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                          <ul className="page_menu_selection">
-                            <li>
-                              <Link to="/">
-                                Menu Item
-                                <i className="fa fa-angle-down" />
-                              </Link>
-                            </li>
-                            <li>
-                              <Link to="/">
-                                Menu Item
-                                <i className="fa fa-angle-down" />
-                              </Link>
-                            </li>
-                            <li>
-                              <Link to="/">
-                                Menu Item
-                                <i className="fa fa-angle-down" />
-                              </Link>
-                            </li>
-                            <li>
-                              <Link to="/">
-                                Menu Item
-                                <i className="fa fa-angle-down" />
-                              </Link>
-                            </li>
-                          </ul>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Menu Item
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Menu Item
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Menu Item
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                      </ul>
-                    </li>
-                    <li className="page_menu_item has-children">
-                      <Link to="/">
-                        Featured Brands
-                        <i className="fa fa-angle-down" />
-                      </Link>
-                      <ul className="page_menu_selection">
-                        <li>
-                          <Link to="/">
-                            Featured Brands
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Menu Item
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Menu Item
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Menu Item
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                      </ul>
-                    </li>
-                    <li className="page_menu_item has-children">
-                      <Link to="/">
-                        Trending Styles
-                        <i className="fa fa-angle-down" />
-                      </Link>
-                      <ul className="page_menu_selection">
-                        <li>
-                          <Link to="/">
-                            Trending Styles
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Menu Item
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Menu Item
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/">
-                            Menu Item
-                            <i className="fa fa-angle-down" />
-                          </Link>
-                        </li>
-                      </ul>
-                    </li>
-                    <li className="page_menu_item">
-                      <a href="blog.html">
-                        blog
-                        <i className="fa fa-angle-down" />
-                      </a>
-                    </li>
-                    <li className="page_menu_item">
-                      <a href="contact.html">
-                        contact
-                        <i className="fa fa-angle-down" />
-                      </a>
-                    </li>
-                  </ul>
-                  <div className="menu_contact">
-                    <div className="menu_contact_item">
-                      <div className="menu_contact_icon">
-                        <img src="images/phone_white.png" alt="" />
-                      </div>
-                      +38 068 005 3570
-                    </div>
-                    <div className="menu_contact_item">
-                      <div className="menu_contact_icon">
-                        <img src="images/mail_white.png" alt="" />
-                      </div>
-                      <a href="mailto:fastsales@gmail.com">
-                        fastsales@gmail.com
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </header>
-    </div>
+    </Fragment>
   );
 };
 
@@ -615,7 +395,7 @@ Header.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  auth: state.auth,
+  isAuthenticated: state.authReducer.isAuthenticated,
 });
 
 export default connect(mapStateToProps, { logout })(Header);
