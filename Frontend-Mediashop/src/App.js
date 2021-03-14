8c59f50753adc7e292b4f108e82b57a749f45216
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-03-14 05:33:08 +0100

    Authentication React Setup

--- Frontend-Mediashop/src/App.js
+++ Frontend-Mediashop/src/App.js
@@ -7,27 +7,43 @@ import ProductList from "./components/products/ProductList";
 import Favorites from "./components/favorites/Favorites";
 import Register from "./components/authentication/Register";
 import Login from "./components/authentication/Login";
+import ResetPassword from "./components/authentication/ResetPassword";
+import ResetPasswordConfirm from "./components/authentication/ResetPasswordConfirm";
+import Activate from "./components/authentication/Activate";
+
 import Alert from "./components/layouts/Alert";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import Layout from "./hocs/Layout";
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <Router>
           <div class="super_container">
-            <Header />
-            <Alert />
-            <Switch>
-              <Route exact path="/product-list" component={ProductList} />
-              <Route exact path="/Favorites" component={Favorites} />
-              <Route exact path="/Register" component={Register} />
-              <Route exact path="/Login" component={Login} />
-            </Switch>
-            <Footer />
+            <Layout>
+              <Alert />
+              <Switch>
+                <Route exact path="/product-list" component={ProductList} />
+                <Route exact path="/Favorites" component={Favorites} />
+                <Route exact path="/Register" component={Register} />
+                <Route exact path="/Login" component={Login} />
+                <Route exact path="/reset-password" component={ResetPassword} />
+                <Route
+                  exact
+                  path="/password/reset/confirm/:uid/:token"
+                  component={ResetPasswordConfirm}
+                />
+                <Route
+                  exact
+                  path="/activate/:uid/:token"
+                  component={Activate}
+                />
+              </Switch>
+            </Layout>
           </div>
         </Router>
       </Provider>
