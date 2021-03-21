c191585f03ea1662d398869563eb43c6c5742374
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-03-21 14:21:33 +0100

    Get category by slug

--- Backend-Mediashop/mediashop_drf/category/urls.py
+++ Backend-Mediashop/mediashop_drf/category/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
          views.category_list.as_view(), name='category_list'),
     path('api/categories/subcategory-list',
          views.subcategory_list.as_view(), name='subcategory_list'),
+    path('api/categories/category/<str:slug>/',
+         views.category_details, name='category_detail'),
 ]
--- Backend-Mediashop/mediashop_drf/category/views.py
+++ Backend-Mediashop/mediashop_drf/category/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import Category
-from .serializers import category_Serializer,subcategory_Serializer
+from .serializers import category_Serializer, subcategory_Serializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 
 class category_list(ListAPIView):
@@ -12,3 +15,17 @@ class category_list(ListAPIView):
 class subcategory_list(ListAPIView):
     queryset = Category.objects.filter(parent__isnull=False)
     serializer_class = subcategory_Serializer
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def category_details(request, slug):
+    """
+    Retrieve, update or delete a category instance.
+    """
+    try:
+        category = Category.objects.get(slug=slug)
+    except Category.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = category_Serializer(category)
+        return Response(serializer.data)
