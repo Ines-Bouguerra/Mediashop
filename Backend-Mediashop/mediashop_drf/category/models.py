03b836cb64543539e3187ea00435a49ab6acfc4b
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-03-30 15:35:08 +0100

    Update Category Model

--- Backend-Mediashop/mediashop_drf/category/models.py
+++ Backend-Mediashop/mediashop_drf/category/models.py
@@ -1,16 +1,18 @@
 from audioop import reverse
-
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 from mptt.fields import TreeForeignKey
 from mptt.models import MPTTModel
 
-
+def upload_to(instance, filename):
+    return 'categories/{filename}'.format(filename=filename)
 class Category(MPTTModel):
     parent = TreeForeignKey('self', on_delete=models.CASCADE,
                             null=True, blank=True, related_name='children')
     name = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(blank=True, upload_to='images/categories/')
+    image = models.ImageField(
+        _("Image"), upload_to=upload_to)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -23,3 +25,5 @@ class Category(MPTTModel):
 
     def __str__(self):
         return self.name
+
+   
