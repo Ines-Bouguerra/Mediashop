ff459b2706f413694c2b0669f4dcf085d11c5a00
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-03-30 15:50:37 +0100

    Fix Product Redux bug & search API

--- Frontend-Mediashop/src/actions/product.js
+++ Frontend-Mediashop/src/actions/product.js
@@ -1,17 +1,12 @@
 import axios from "axios"
-import { GET_PRODUCT, PRODUCT_ERROR } from "./types"
+import { GET_PRODUCT, PRODUCT_ERROR, SEARCH_PRODUCT, SEARCH_PRODUCT_ERROR } from "./types"
 
 
 //Get Product
 
-export const getProduct = () => async (dispatch, sort, order, limit) => {
+export const getProduct = () => async (dispatch) => {
   try {
-    const res = await axios.get('http://localhost:8080/api/products/product_list', {
-      sort,
-      order,
-      limit,
-    })
-
+    const res = await axios.get('http://localhost:8080/api/products/product_list')
     dispatch({
       type: GET_PRODUCT,
       payload: res.data,
@@ -27,8 +22,36 @@ export const getProduct = () => async (dispatch, sort, order, limit) => {
     })
   }
 }
-export const searchProduct = (keyword = '') => async () => {
 
-  await axios.get(`http://127.0.0.1:8080/api/products/search_view?keyword=${keyword}`)
 
+export const searchProduct = q => async dispatch => {
+  try {
+    const res = await axios.get(`http://127.0.0.1:8080/api/products/search_view?q=${q}`)
+    dispatch({
+      type: SEARCH_PRODUCT,
+      payload: res.data,
+    })
+  } catch (error) {
+    dispatch({
+      type: SEARCH_PRODUCT_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    })
+  }
 }
+
+export const filterProducts = (q) => async (dispatch) => {
+  const res = await axios.get(`http://127.0.0.1:8080/api/products/search_view?q=${q}`)
+  dispatch({
+    type: SEARCH_PRODUCT,
+    payload: {
+
+      products: res.data,
+
+    },
+  });
+};
+
+
--- Frontend-Mediashop/src/reducers/index.js
+++ Frontend-Mediashop/src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
 import authReducer from "./authReducer";
-import product from "./product";
 import category from "./category";
+import product from "./product";
 
 export default combineReducers({
   alert,
