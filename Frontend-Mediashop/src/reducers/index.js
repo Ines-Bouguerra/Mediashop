4ce0bf3180e1b3ca81c52ff2674d858c8d441c5b
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-04-01 00:51:41 +0100

    Add Product List & Detail reducer

--- Frontend-Mediashop/src/reducers/index.js
+++ Frontend-Mediashop/src/reducers/index.js
@@ -1,12 +1,12 @@
-import { combineReducers } from "redux";
-import alert from "./alert";
-import authReducer from "./authReducer";
-import category from "./category";
-import product from "./product";
-
+import { combineReducers } from 'redux';
+import alert from './alert';
+import authReducer from './authReducer';
+import category from './category';
+import {productDetailsReducer, productListReducer} from './product';
 export default combineReducers({
   alert,
   authReducer,
-  product,
+  productList:productListReducer,
+  productDetails:productDetailsReducer,
   category,
 });
