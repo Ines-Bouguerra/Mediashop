737f6f6828e45b71e710fa53d163d86ff7f6f6da
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-04-01 00:53:54 +0100

    Add Product component

---
+++ Frontend-Mediashop/src/components/products/Product.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const Product = ({ product, category, loading }) => {
+  return (
+    <>
+      <div className="product_border" />
+      <div className="product_image d-flex flex-column align-items-center justify-content-center">
+        <Link to={`/product/${product._id}`}>
+          <img src={product.image} alt="" />
+        </Link>
+      </div>
+      <div className="product_content">
+        <div className="product_price">{product.price} TN</div>
+        <div className="product_name">
+          <div>
+            <Link to={`/product/${product._id}`}>{product.name}</Link>
+          </div>
+        </div>
+      </div>
+      <div className="product_fav">
+        <i className="fas fa-heart" />
+      </div>
+      <ul className="product_marks">
+        <li className="product_mark product_new product_discount">
+          {product.discount}
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default Product
