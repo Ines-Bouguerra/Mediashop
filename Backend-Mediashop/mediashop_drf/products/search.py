75245fae9857fc110f6cd8ce5a3227b5e282a5a7
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-04-01 04:10:37 +0100

    Add discount & image fields to search with Elasticsearch

--- Backend-Mediashop/mediashop_drf/products/search.py
+++ Backend-Mediashop/mediashop_drf/products/search.py
@@ -7,26 +7,28 @@ ELASTIC_HOST = 'http://localhost:9200/'
 client = elasticsearch.Elasticsearch(hosts=[ELASTIC_HOST])
 INDEXES = ['mediashop_products']
 fields = ['name', 'reference', 'short_description',
-          "description",  "sub_category", "brand", "priceString"]
+          'description',  'sub_category', 'brand', 'priceString','image','discount']
 
 
 def lookup(query, index=INDEXES, fields=fields):
     if not query:
         return
     results = Search(index=index).using(client).query(
-        "multi_match", fields=fields, fuzziness="AUTO", query=query)
+        'multi_match', fields=fields, fuzziness='AUTO', query=query)
     q_results = []
     for result in results:
         print(result.name)
-        print(result.reference)
+        print(result.image)
         data = {
-            "reference": result.reference,
-            "sub_category": result.sub_category,
-            "name": result.name,
-            "price": result.priceString,
-            "brand": result.brand,
-            "short_description": result.short_description,
-            "description": result.description
+            'reference': result.reference,
+            'sub_category': result.sub_category,
+            'name': result.name,
+            'price': result.priceString,
+            'brand': result.brand,
+            'short_description': result.short_description,
+            'description': result.description,
+            'image': result.image,
+            'discount': result.discount,
         }
         q_results.append(data)
     return q_results
