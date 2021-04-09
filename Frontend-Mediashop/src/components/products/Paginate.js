0d36ffed04a2785a812a920645a4438351e6d072
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-04-09 21:12:58 +0100

    update pagination

--- Frontend-Mediashop/src/components/products/Paginate.js
+++ Frontend-Mediashop/src/components/products/Paginate.js
@@ -5,8 +5,8 @@ import { LinkContainer } from 'react-router-bootstrap'
 const Paginate = ({ pages, page, query = '' }) => {
     return (
         pages > 1 && (
-            <Pagination class="wishlist d-flex flex-row align-items-center justify-content-end">
-                <li className="page-item m-1">
+            <Pagination class="d-flex flex-row  -contenjustifyt-end">
+                <li className="page_nav m-1">
                     <a class="page-link" href="#," aria-label="Previous">
                         <span aria-hidden="true">&laquo;</span>
                         <span className="sr-only">Previous</span>
@@ -24,7 +24,7 @@ const Paginate = ({ pages, page, query = '' }) => {
 
                     </li>
                 ))}
-                <li className="page-item m-1">
+                <li className="page_nav m-1">
                     <a className="page-link" href="#," aria-label="Next">
                         <span aria-hidden="true">&raquo;</span>
                         <span className="sr-only">Next</span>
