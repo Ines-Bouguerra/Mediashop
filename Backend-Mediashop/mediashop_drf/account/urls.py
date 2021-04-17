ff791cef02589d155581e9113bba602d0eaf1e2d
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-04-17 11:19:36 +0100

    Add wishList backe,d code

--- Backend-Mediashop/mediashop_drf/account/urls.py
+++ Backend-Mediashop/mediashop_drf/account/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
-from account.views import registration_view
-
-
+from account.views import registration_view, wishlist, add_to_wishlist
+from django.urls import path
+from . import views
 app_name = "account"
 urlpatterns = [
-    url(r'^api/authentication/register$', registration_view, name='registration_view'),
+    url(r'^api/authentication/register$',
+        registration_view, name='registration_view'),
+    # Wish List
+    path("wishlist", views.wishlist, name="wishlist"),
+    path("wishlist/add_to_wishlist/<int:id>",
+         views.add_to_wishlist, name="user_wishlist"),
 ]
--- Backend-Mediashop/mediashop_drf/account/views.py
+++ Backend-Mediashop/mediashop_drf/account/views.py
@@ -4,8 +4,12 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from django.contrib.auth.decorators import login_required
 from account.serializers import RegistrationSerializer
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from products.models import Product
+from django.urls import reverse
 
 
 @api_view(['POST', ])
@@ -22,3 +26,22 @@ def registration_view(request):
         else:
             data = serializer.errors
         return Response(data)
+
+
+def wishlist(request):
+    products = Product.objects.filter(users_wishlist=request.user)
+    return JsonResponse({"wishlist": products})
+
+
+@login_required
+def add_to_wishlist(request, id):
+    product = get_object_or_404(Product, id=id)
+    if product.users_wishlist.filter(id=request.user.id).exists():
+        product.users_wishlist.remove(request.user)
+        messages.success(request, product.name +
+                         " has been removed from your WishList")
+    else:
+        product.users_wishlist.add(request.user)
+        messages.success(request, "Added " +
+                         product.name + " to your WishList")
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
