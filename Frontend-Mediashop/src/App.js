94a0ce9cf85ecffb3aa2dc747876b6007593b80b
bilel <bilel1996k@gmail.com>
2021-04-17 15:49:47 +0100

    add default layout

--- Frontend-Mediashop/src/App.js
+++ Frontend-Mediashop/src/App.js
@@ -1,59 +1,67 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Activate from './components/authentication/Activate'
-import Login from './components/authentication/Login'
-import Register from './components/authentication/Register'
-import ResetPassword from './components/authentication/ResetPassword'
-import ResetPasswordConfirm from './components/authentication/ResetPasswordConfirm'
-import CategoryScreen from './screens/client/CategoryScreen'
-import Favorites from './components/favorites/Favorites'
-import Home from './components/home/Home'
-import Alert from './components/layouts/Alert'
-import Layout from './hocs/Layout'
-import HomeScreen from './screens/client/HomeScreen'
-import ProductScreen from './screens/client/ProductScreen'
-import store from './store'
-import Google from './components/authentication/Google'
-import CompareScreen from './screens/client/CompareScreen'
-import HeaderAdmin from "./components/header/HeaderAdmin"
-import UserRoute from './components/routing/UserRoute'
+/** @format */
 
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Activate from "./components/authentication/Activate";
+import Login from "./components/authentication/Login";
+import Register from "./components/authentication/Register";
+import ResetPassword from "./components/authentication/ResetPassword";
+import ResetPasswordConfirm from "./components/authentication/ResetPasswordConfirm";
+import CategoryScreen from "./screens/client/CategoryScreen";
+import Favorites from "./components/favorites/Favorites";
+import Home from "./components/home/Home";
+import Alert from "./components/layouts/Alert";
+import Layout from "./hocs/Layout";
+import HomeScreen from "./screens/client/HomeScreen";
+import ProductScreen from "./screens/client/ProductScreen";
+import store from "./store";
+import Google from "./components/authentication/Google";
+import CompareScreen from "./screens/client/CompareScreen";
+import HeaderAdmin from "./components/header/HeaderAdmin";
+import UserRoute from "./components/routing/UserRoute";
+import AdminLayout from "./hocs/adminLayout";
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <Router>
-          <div class='super_container'>
-            <Layout>
-              <Alert />
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <UserRoute exact path='/wishlist' component={Favorites} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-                <Route exact path='/google' component={Google} />
-                <Route exact path='/reset-password' component={ResetPassword} />
-                <Route
-                  path='/password/reset/confirm/:uid/:token'
-                  component={ResetPasswordConfirm}
-                />
-                <Route path='/activate/:uid/:token' component={Activate} />
-                <Route path='/category/:slug' component={CategoryScreen} />
-                <Route exact path='/product-list' component={HomeScreen} />
-                <Route exact path='/search/:query' component={HomeScreen} />
-                <Route exact path='/page/:pageNumber' component={HomeScreen} />
-                <Route exact path='/search/:query/page/:pageNumber' component={HomeScreen} />
-                <Route path='/product/:id' component={ProductScreen} />
-                <Route path='/compare/:name/:reference/:price' component={CompareScreen} />
-              </Switch>
-            </Layout>
+          <div class="super_container">
+            <Switch>
+            <Route path="/admin/:path?" exact>
+                <AdminLayout>
+                  <Switch>
+                    <Route exact path="/admin" component={HeaderAdmin} />{" "}
+                  </Switch>
+                </AdminLayout>
+              </Route>
+              <Route path="" exact>
+                <Layout>
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                    <UserRoute exact path="/wishlist" component={Favorites} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/google" component={Google} />
+                    <Route exact path="/reset-password" component={ResetPassword} />
+                    <Route path="/password/reset/confirm/:uid/:token" component={ResetPasswordConfirm} />
+                    <Route path="/activate/:uid/:token" component={Activate} />
+                    <Route path="/category/:slug" component={CategoryScreen} />
+                    <Route exact path="/product-list" component={HomeScreen} />
+                    <Route exact path="/search/:query" component={HomeScreen} />
+                    <Route exact path="/page/:pageNumber" component={HomeScreen} />
+                    <Route exact path="/search/:query/page/:pageNumber" component={HomeScreen} />
+                    <Route path="/product/:id" component={ProductScreen} />
+                    <Route path="/compare/:name/:reference/:price" component={CompareScreen} />
+                  </Switch>
+                </Layout>
+              </Route>
+            </Switch>
           </div>
-          <Route exact path='/admin/header' component={HeaderAdmin}/>
         </Router>
       </Provider>
-    )
+    );
   }
 }
 
-export default App
+export default App;
