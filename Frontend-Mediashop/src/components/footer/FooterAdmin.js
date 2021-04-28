2fb31f5f1aebbe26964cc116cc2dbaf2a9835ddf
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-04-29 00:01:09 +0100

    add admin footer

--- Frontend-Mediashop/src/components/footer/FooterAdmin.js
+++ Frontend-Mediashop/src/components/footer/FooterAdmin.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { MDBFooter } from 'mdb-react-ui-kit'
 
-export const FooterAdmin = () => {
+const FooterAdmin = () => {
     return (
-        <div>
-            
+        <MDBFooter backgroundColor='light' className='text-center text-lg-left'>          
+          <div className='text-white text-center p-3' style={{ backgroundColor: '#0097A7' }}>
+            &copy; {new Date().getFullYear()} Copyright:{' '}
+            <a className='text-white' href='https://www.medianet.tn/fr'>
+                Mediashop.tn
+        </a>
         </div>
+        </MDBFooter>
     )
 }
+export default FooterAdmin
