bf0a5eba1423db714b504d638f457acb9f6300f3
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-05-22 21:10:42 +0100

    add power BI report to admin web site

--- Frontend-Mediashop/src/components/admin/Product.js
+++ Frontend-Mediashop/src/components/admin/Product.js
@@ -20,51 +20,54 @@ const Product = ({ match }) => {
         )
     }, [dispatch, query, pageNumber])
     return (
-        <MDBRow className='row-cols-1 row-cols-md-5 g-4  m-5'>
+        <>
+            <MDBRow className='row-cols-1 row-cols-md-5 g-4  m-5'>
 
 
-            {loading ? (
-                <Loader />
-            ) : (
-                <>
-                    {products.map((product) => (
-                        <div
-                            key={product.id}
-                        >
+                {loading ? (
+                    <Loader />
+                ) : (
+                    <>
+                        {products.map((product) => (
+                            <div
+                                key={product.id}
+                            >
 
-                            <MDBCol>
-                                <MDBCard shadow='0' border='info' style={{ maxWidth: '14rem' }}>
-                                    <MDBCardImage
-                                        src={product.image}
-                                        alt='...'
-                                        position='top'
-                                    />
-                                    <MDBCardBody>
-                                        <MDBCardTitle>{product.name}</MDBCardTitle>
-                                        <MDBCardText>
-                                            {product.name}
-                                        </MDBCardText>
-                                    </MDBCardBody>
-                                    <MDBCardFooter background='info' >
-                                        <MDBBtn className='ms-2 m-3' tag='a' color='warning' floating>
-                                        <i class="fas fa-plus-circle"></i>
-                                        </MDBBtn>
-                                        <MDBBtn className='ms-2 m-3' tag='a' color='warning' floating>
-                                        <i class="fas fa-edit"></i>
-                                        </MDBBtn><MDBBtn className='ms-2 m-3' tag='a' color='warning' floating>
-                                            <MDBIcon class="fas fa-trash" />
-                                        </MDBBtn>
-                                        <br/>
-                                        <small className='text-white m-3'>Last updated 3 mins ago</small>
-                                    </MDBCardFooter>
-                                </MDBCard>
-                            </MDBCol> </div>
+                                <MDBCol>
+                                    <MDBCard shadow='0' border='info' style={{ maxWidth: '14rem' }}>
+                                        <MDBCardImage
+                                            src={product.image}
+                                            alt='...'
+                                            position='top'
+                                        />
+                                        <MDBCardBody>
+                                            <MDBCardTitle>{product.name}</MDBCardTitle>
+                                            <MDBCardText>
+                                                {product.name}
+                                            </MDBCardText>
+                                        </MDBCardBody>
+                                        <MDBCardFooter background='info' >
+                                            <MDBBtn className='ms-2 m-3' tag='a' color='warning' floating>
+                                                <i class="fas fa-plus-circle"></i>
+                                            </MDBBtn>
+                                            <MDBBtn className='ms-2 m-3' tag='a' color='warning' floating>
+                                                <i class="fas fa-edit"></i>
+                                            </MDBBtn><MDBBtn className='ms-2 m-3' tag='a' color='warning' floating>
+                                                <MDBIcon class="fas fa-trash" />
+                                            </MDBBtn>
+                                            <br />
+                                            <small className='text-white m-3'>Last updated 3 mins ago</small>
+                                        </MDBCardFooter>
+                                    </MDBCard>
+                                </MDBCol> </div>
 
-                    ))}
-                </>)}
+                        ))}
+                    </>)}
 
-
-        </MDBRow>
+            </MDBRow>
+            <iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZmEwNDdhZTktMzlmYi00NjNmLWEzOTUtN2MzMGIyOGRiYmY1IiwidCI6Ijg5MTRmYmNhLTBiODctNGFiNS1iMzJhLTUxMzU1Zjk2Y2Y3NyJ9&embedImagePlaceholder=true&pageName=ReportSection" frameborder="0" allowFullScreen="true" alt="Mediasop Repport"></iframe>
+        </>
     )
 }
+
 export default Product
