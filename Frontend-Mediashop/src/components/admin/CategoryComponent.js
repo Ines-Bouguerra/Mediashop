a69f48fb130a8e0c191e83bdaf498f8abab3b750
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-05-30 23:45:16 +0100

    Add Category component

---
+++ Frontend-Mediashop/src/components/admin/CategoryComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class CategoryComponent extends Component {
+    render() {
+        return (
+            <section className="content">
+            <div className="container-fluid">
+              <div className="block-header">
+                <h2>category</h2>
+              </div>
+            </div>
+          </section>
+        )
+    }
+}
