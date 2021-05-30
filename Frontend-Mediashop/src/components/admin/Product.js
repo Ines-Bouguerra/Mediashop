3ee112905f3ca829539501a70efe3b0a885e73b8
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-05-30 23:46:07 +0100

    Add Product  Admin  component

--- Frontend-Mediashop/src/components/admin/Product.js
+++ Frontend-Mediashop/src/components/admin/Product.js
@@ -1,59 +1,55 @@
-import React, { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { getProduct } from '../../actions/product';
-import Loader from '../Loader';
-import { Button, Row, Card } from 'react-bootstrap';
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProduct } from "../../actions/product";
+import Loader from "../Loader";
 
 const Product = ({ match }) => {
-    const dispatch = useDispatch()
-    const query = match.params.query
-    const pageNumber = match.params.pageNumber || 1
-    const productList = useSelector((state) => state.productList)
-    const { loading, products, page, pages } = productList
-    useEffect(() => {
-        dispatch(
-            getProduct(query, pageNumber),
+  const dispatch = useDispatch();
+  const query = match.params.query;
+  const pageNumber = match.params.pageNumber || 1;
+  const productList = useSelector((state) => state.productList);
+  const { loading, products } = productList;
+  useEffect(() => {
+    dispatch(getProduct(query, pageNumber));
+  }, [dispatch, query, pageNumber]);
+  return (
+    <section className="content">
+      <div className="container-fluid">
+        <div className="block-header">
+          <h2>Mediashop Products</h2>
+        </div>
 
-        )
-    }, [dispatch, query, pageNumber])
-    return (
-        <>
-            <Row className='row-cols-1 row-cols-md-5 g-4  m-5'>
-                {loading ? (
-                    <Loader />
-                ) : (
-                    <>
-                        {products.map((product) => (
-                            <div
-                                key={product.id}
-                            >
+        <div className="row clearfix">
+          <div className="col-md-3 mt-3">
+            {loading ? (
+              <Loader />
+            ) : (
+              <>
+                {products.map((product) => (
+                  <div key={product.id}>
+                    <div className="card">
+                      <div className="header bg-cyan">
+                        <h2>
+                          {product.name} - {product.priceString} TND
+                          <small>{product.marketplace}</small>
+                        </h2>
+                      </div>
+                      <img alt="" variant="top" src={product.image} style={{width:250,height:250}}/>
 
-                                <Card style={{ width: '18rem' }}>
-                                    <Card.Img variant="top" src={product.image} />
-                                    <Card.Body>
-                                        <Card.Title>{product.name}</Card.Title>
-                                        <Card.Text>
-                                            {product.description}
-                                        </Card.Text>
-                                        <div>
-                                            <Button variant="primary" size="sm">   <i class="fas fa-edit"></i></Button>
-                                            <Button variant="primary" size="sm">   <i class="fas fa-trash"></i></Button>
-                                            <Button variant="primary" size="sm">   <i class="fas fa-info"></i></Button>
-                                        </div>
-                                    </Card.Body>
-                                    <Card.Footer className="text-muted text-center">{product.timestamp}</Card.Footer>
-                                </Card>
+                      <div className="body align-center">
+                        {product.description}
+                        <p class="font-overline col-pink">Last updated : {product.timestamp}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-                            </div>
-
-                        ))}
-                    </>)}
-
-            </Row>
-            <iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZmEwNDdhZTktMzlmYi00NjNmLWEzOTUtN2MzMGIyOGRiYmY1IiwidCI6Ijg5MTRmYmNhLTBiODctNGFiNS1iMzJhLTUxMzU1Zjk2Y2Y3NyJ9&embedImagePlaceholder=true&pageName=ReportSection" frameborder="0" allowFullScreen="true" alt="Mediasop Repport"></iframe>
-        </>
-    )
-}
-
-export default Product
+export default Product;
