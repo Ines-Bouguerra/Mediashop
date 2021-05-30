13266c13931ed134f6eb36ab09585b5f054cb37b
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-05-30 23:42:43 +0100

    Power BI kmeans

---
+++ Frontend-Mediashop/src/components/admin/PowerBIDashbordKmeans.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class PowerBIDashbordKmeans extends Component {
+  render() {
+    return (
+      <section>
+        <iframe
+          width="1186"
+          height="390"
+          src="https://app.powerbi.com/view?r=eyJrIjoiZmEwNDdhZTktMzlmYi00NjNmLWEzOTUtN2MzMGIyOGRiYmY1IiwidCI6Ijg5MTRmYmNhLTBiODctNGFiNS1iMzJhLTUxMzU1Zjk2Y2Y3NyJ9&embedImagePlaceholder=true&pageName=ReportSection"
+          frameborder="0"
+          allowFullScreen="true"
+          alt="Mediasop Repport"
+        />
+      </section>
+    );
+  }
+}
