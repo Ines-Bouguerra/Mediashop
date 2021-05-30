eda504442814fea2de609e67fa9d4d3a9ea179d1
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-05-30 23:44:05 +0100

    Add Contact component

---
+++ Frontend-Mediashop/src/components/admin/ContactComponent copy 2.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class ContactComponent extends Component {
+  render() {
+    return (
+      <section className="content">
+        <div className="container-fluid">
+          <div className="block-header">
+            <h2>contact</h2>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
