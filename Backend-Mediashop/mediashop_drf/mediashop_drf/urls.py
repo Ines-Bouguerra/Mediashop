cc585982b265594c7240d5ae47621891c0d9b925
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-05-30 03:56:38 +0100

    get token API

--- Backend-Mediashop/mediashop_drf/mediashop_drf/urls.py
+++ Backend-Mediashop/mediashop_drf/mediashop_drf/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
-# from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+
+# from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
@@ -30,15 +31,17 @@ urlpatterns = [
     url(r'^', include('contact.urls')),
     url(r'^', include('brand.urls')),
     path('admin/', admin.site.urls),
-    # path('authentication/', obtain_auth_token),
+    # path('api/gettoken/', jwt_views.TokenObtainPairView.as_view(),
+    #      name='token_obtain_pair'),
+    # path('api/refresh_token/', jwt_views.TokenRefreshView.as_view(),
+    #      name='token_refresh'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    url(r'^auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.social.urls')),
 
 
 ]
-
-
 urlpatterns += [re_path(r'^.*',
                         TemplateView.as_view(template_name='index.html'))]
 
--- Backend-Mediashop/mediashop_drf/post/views.py
+++ Backend-Mediashop/mediashop_drf/post/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from post.models import Post
 from post.serializers import PostSerializer
@@ -8,6 +10,9 @@ class PostListCreateAPIView(ListCreateAPIView):
     """
     API view to retrieve list of posts or create new
     """
+    # authentication_classes = (JWTAuthentication,)
+    # permission_classes = (IsAuthenticated,)
+
     serializer_class = PostSerializer
     queryset = Post.objects.all()
 
