457eab317bd9cb84e794a7f10dfaa58e7a7829a5
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-05-31 21:41:50 +0100

    Update Category Component

--- Frontend-Mediashop/src/components/admin/CategoryComponent.js
+++ Frontend-Mediashop/src/components/admin/CategoryComponent.js
@@ -1,15 +1,57 @@
-import React, { Component } from 'react'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { getCategories } from "../../actions/category";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../Loader";
+const CategoryComponent = () => {
+  const dispatch = useDispatch();
 
-export default class CategoryComponent extends Component {
-    render() {
-        return (
-            <section className="content">
-            <div className="container-fluid">
-              <div className="block-header">
-                <h2>category</h2>
-              </div>
+  const categoryList = useSelector((state) => state.categoryList);
+  const { loading, categories } = categoryList;
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
+
+  
+
+  return (
+    <section className="content">
+      <div className="container-fluid">
+        <div className="block-header">
+          <div className="container_cat_home">
+            <div className="title_cat_home font-underline font-bold col-pink align-center">
+              Search by category
             </div>
-          </section>
-        )
-    }
-}
+            <div className="row d-flex">
+              {loading ? (
+                <Loader />
+              ) : (
+                <div class="row">
+                  {categories.map((category) => (
+                    <div
+                      className="col-xs-12 col-sm-4 col-md-3 col-lg-2 cat_product_home"
+                      key={category.id}
+                    >
+                      <a
+                        href="ordinateur-portable"
+                        className="elem_product_home"
+                      >
+                        <div className="img_cat">
+                          <img src={category.image} alt="" />
+                        </div>
+                        <h3 className="title_cat">{category.name}</h3>
+                        <span className="articles_cat">? products </span>
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CategoryComponent;
