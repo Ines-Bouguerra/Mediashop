73642d132263d2f751c7b316db94274b4b7a21c6
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-06-03 14:05:15 +0100

    Update Wishlist Model

--- Backend-Mediashop/products/models.py
+++ Backend-Mediashop/products/models.py
@@ -34,10 +34,12 @@ class Product(models.Model):
 
 
 class WishlistItem(models.Model):
-    id = models.AutoField(primary_key=True)
+    session_id = models.CharField(null=True, max_length=32)
     product = models.ForeignKey(
         Product, null=True, on_delete=models.SET_NULL)
     user = models.ForeignKey(
         Account, on_delete=models.CASCADE, blank=True, null=True)
-    created_at = models.DateField(auto_now_add=True,blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True, blank=True, null=True)
 
+    def __str__(self):
+        return self.session_id
